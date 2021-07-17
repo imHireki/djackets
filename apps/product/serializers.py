260a20f64b7eb04af965cda114cf9d9068a6162d
imHireki <imhireki@gmail.com>
2021-07-17 15:05:02 -0300

    add latest products

--- apps/product/serializers.py
+++ apps/product/serializers.py
@@ -7,6 +7,7 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
         fields = [
+            'id',
             'name',
             'description',
             'price',
