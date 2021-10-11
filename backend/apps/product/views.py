694545a99b3aa7894d98bbc2a8368c7498677ae9
imHireki <imhireki@gmail.com>
2021-10-11 14:54:01 -0300

    feat: add product detail

--- backend/apps/product/views.py
+++ backend/apps/product/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -8,8 +10,20 @@ from .models import Product, Category
 class LatestProductList(APIView):
     def get(self, *args, **kwargs):
         products = Product.objects.all()
-        serializer = ProductSerializer(data=products, many=True)
-        serializer.is_valid()
-        serializer.save()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(
+                category__slug=category_slug
+            ).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+ 
+    def get(self, request, category_slug, product_slug, *args, **kwargs):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
         return Response(serializer.data)
 
