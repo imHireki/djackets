23099a90ccb70e62d8da773a2d81a0bfbf22f00f
imHireki <imhireki@gmail.com>
2021-10-12 10:23:26 -0300

    feat: add 'addToCart' function

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -26,7 +26,7 @@
 
             <router-link to="/cart" class="button is-success">
               <span class="icon"><i class="fas fa-shopping-cart"></i></span>
-              <span>Cart</span>
+                <span>Cart ({{ cartTotalLength }})</span>
             </router-link>
           </div>
         </div>
@@ -50,6 +50,27 @@ export default {
   data() {
     return {
       showMobileMenu: false,
+      cart: {
+        items: []
+      }
+    }
+  },
+  beforeCreate() {
+    this.$store.commit('initializeStore')
+  },
+  mounted() {
+    this.cart = this.$store.state.cart
+  },
+  computed: {
+    cartTotalLength() {
+      let totalLength = 0
+
+      // loop thru the products and add its quantities
+      for (let i = 0; i < this.cart.items.length; i++) {
+        totalLength += this.cart.items[i].quantity
+      }
+
+      return totalLength
     }
   }
 }
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,8 +2,36 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    cart: {
+        items: [],
+    },
+    isAuthenticated: false,
+    token: '',
+    isLoading: false
   },
   mutations: {
+    initializeStore(state) {
+      // state.cart = localstorage cart or create one
+      if (localStorage.getItem('cart')) {
+        state.cart = JSON.parse(localStorage.getItem('cart'))
+      } else {
+        localStorage.setItem('cart', JSON.stringify(state.cart))
+      } 
+    },
+    addToCart(state, item) {
+      // cart product
+      const exists = state.cart.items.filter(i => i.product.id === item.product.id)
+
+      // add input quantity or add item(product)
+      if (exists.length) {
+        exists[0].quantity = parseInt(exists[0].quantity) + parseInt(item.quantity)
+      } else {
+        state.cart.items.push(item)
+      }
+
+      // update localStorage
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    }
   },
   actions: {
   },
