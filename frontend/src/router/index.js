6f436df02d31a2c862d2e535b040874b8180acd1
imHireki <imhireki@gmail.com>
2021-10-15 17:30:07 -0300

    feat: add my account page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
+
 import Home from '../views/Home.vue'
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
@@ -6,6 +8,7 @@ import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
+import MyAccount from '../views/MyAccount.vue'
 
 
 const routes = [
@@ -21,16 +24,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/:category_slug/:product_slug/',
-    name: 'Product',
-    component: Product,
-  },
-  {
-    path: '/:category_slug/',
-    name: 'Category',
-    component: Category,
   },
   {
     path: '/search',
@@ -52,11 +45,37 @@ const routes = [
     name: 'LogIn',
     component: LogIn,
   },
+  {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin:  true
+    }
+  },
+  {
+    path: '/:category_slug/:product_slug/',
+    name: 'Product',
+    component: Product,
+  },
+  {
+    path: '/:category_slug/',
+    name: 'Category',
+    component: Category,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'LogIn', query: {to: to.path} });
+  } else {
+    next()
+  }
 })
 
 export default router
